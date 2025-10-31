d785d532b380f1373cffafaa419ff15599823113
Isturfore <130397874+Isturfore@users.noreply.github.com>
2025-10-31 13:08:29 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -69,15 +69,15 @@ fn pl_choose(rng: &mut ThreadRng, vec: &mut Vec<i32>, pl_cards: &mut i32) -> boo
 fn main() {
     let mut rng: ThreadRng = rand::thread_rng();
 
-    let mut vec = vec![1,2,3,4,5,6,7,8,9,10,11];
-    let mut pl_cards = 0;
-    let mut dl_cards = 0;
+    let mut vec: Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10,11];
+    let mut pl_cards: i32 = 0;
+    let mut dl_cards: i32 = 0;
 
     init(&mut rng, &mut vec, &mut pl_cards, &mut dl_cards);
 
     while !vec.is_empty() {
-        let dl_res = dl_choose(&mut rng, &mut vec, &mut dl_cards);
-        let pl_res = pl_choose(&mut rng, &mut vec, &mut pl_cards);
+        let dl_res: bool = dl_choose(&mut rng, &mut vec, &mut dl_cards);
+        let pl_res: bool = pl_choose(&mut rng, &mut vec, &mut pl_cards);
 
         if pl_cards > 21 {
             println!("Вы перебрали! Игра окончена.");
@@ -98,7 +98,7 @@ fn main() {
     else if pl_cards > dl_cards && pl_cards <= 21 {
         println!("Вы победили. Количество карт дилера: {}, Ваше количество карт: {}", dl_cards, pl_cards);
     }
-    else if dl_cards > pl_cards && dl_cards <= 21{
+    else if dl_cards > pl_cards && dl_cards <= 21 {
         println!("Победил дилер. Его количество карт: {}, Ваше количество карт: {}.", dl_cards, pl_cards);
     }
 }
